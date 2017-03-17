5367422f283cb1ab6c0d820e50805200c55f955f
Michael Pascuzzi <michael@theimaginationhouse.com>
2017-03-17 12:20:38 -0400

    Hooked up add note to flux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
 
 class App extends React.Component {
   render() {
@@ -20,11 +21,9 @@ class App extends React.Component {
     );
   }
   addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
+    this.props.NoteActions.create({
+      id: uuid.v4(),
+      task: 'New task'
     });
   }
   deleteNote = (id, e) => {
@@ -62,4 +61,6 @@ class App extends React.Component {
 
 export default connect(({notes}) => ({
   notes
-}))(App)
+}), {
+  NoteActions
+})(App)
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,7 +1,10 @@
 import uuid from 'uuid'
+import NoteActions from '../actions/NoteActions';
 
 export default class NoteStore {
   constructor() {
+    this.bindActions(NoteActions);
+
     this.notes = [
       {
         id: uuid.v4(),
@@ -12,5 +15,16 @@ export default class NoteStore {
         task: 'Do laundry'
       }
     ];
+  }
+  create(note) {
+    this.setState({
+      notes: this.notes.concat(note)
+    });
+  }
+  update(updatedNote) {
+    console.log('update note', updatedNote);
+  }
+  delete(id) {
+    console.log('delete note', id);
   }
 }
